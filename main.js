a8edf2e3b401925ece759b1b421c43ded6399f94
Matt <ongmattd@gmail.com>
2016-09-08 03:24:26 -1000

    clean up

--- main.js
+++ main.js
@@ -6,16 +6,15 @@
  * @copyright Copyright (C) 2015, Damian Worsdell
  */
 
-var weatherData;
 
 /* Forecast.io API Variables (Weather Forecasting) */
 var FORECAST_URL = 	"https://api.forecast.io/forecast/";
 var FORECAST_API = 	"f33d413250b6d07e6fa54b0a0d1bd5fa";	// Your API Key
 var latitude     =	"45.5898";	// Your Latitude
-var longitude    =	"-122.5951";	// Your Longitude
-var year         =  "2016"
-var month        =	"06"
-var day          =	"00"
+var longitude    =	"-122.5951";// Your Longitude
+var year         =  "2016"//your year
+var month        =	"06"//your month
+var day          =	"00"//your day (starts at 0 and increments to 30)
 var acOn 		 =  0;
 var heaterOn 	 =  0;
 var dateArr      =  [];
@@ -23,29 +22,21 @@ var acArr 	     =  [];
 var heatArr		 = 	[];
 var dayCounter	 =	1;
 
-function exeForecast(){
-	fetchWeather("fetching", function(){
-		// setTimeout(function(){console.log("wait 5 seconds");}, 5000);
-	});
-}
 
 /* Function: Fetch Forecast.io weather forecast */
-function fetchWeather(param, callback) {
+function fetchWeather() {
 	if(dayCounter<= 30){
-	// for (j = 0; j < 30; j++){//for each day
-		// if (j>1){
-		// 	// setTimeout(, 3000);
-		// }
-		//iterate through day
+
+		//iterate days
 		day = incString(day);
 
-		//call api to get data for day
+		//format of ajax call borrowed from https://github.com/damian-w/forecast.io-javascript-jsonp-api/blob/master/src/main.js
 		$.ajax({
 			url: FORECAST_URL + FORECAST_API + '/' + latitude + ',' + longitude + "," + year +"-" + month + "-" + day + "T00:00:00" +"?units=auto",
 			dataType: "jsonp",
-			success: function (data) { weatherData = data;	/* Store our newly aquired weather data */
+			success: function (data) {
 
-				//check each our fo the day and increment as and heater usage for the 24 hours
+				//check each hour of the day and increment a/c and heater usage, for the 24 hours
 				for (i = 0; i < data.hourly.data.length; i++){
 					if (data.hourly.data[i].temperature > 75){
 						acOn = acOn+1;
@@ -57,29 +48,24 @@ function fetchWeather(param, callback) {
 
 				//push all data to arrays for later use
 				dateArr.push(year+"-"+month+"-"+day);
-				// $('#ac').html('and the ac was turned on ' + acOn + " times.");
-				console.log("ac "+acOn);
 				acArr.push(acOn);
-				// $('#heater').html('and the heater was turned on ' + heaterOn + " times.");
-				console.log("heater "+heaterOn);
 				heatArr.push(heaterOn);
+
+				//post to html
 				document.getElementById("day"+dayCounter.toString()).innerHTML = dateArr[dayCounter-1] +"<br>"+ "A/C: "+acArr[dayCounter-1]+ "<br>"+"Heater: "+heatArr[dayCounter-1];
 				dayCounter++;
+
 				//reset ac and heater counters
 				acOn = 0;
 				heaterOn = 0;
 			}
 
 		});
-	// 	setTimeout(function() { console.log("done with one call");  }, 3000);
 	}
 	else{
 		alert("There is no more data to display")
 	}
 
-	if (callback && typeof(callback) === "function") {
-        callback();
-    }
 	// Fetch the weather every 2 seconds
 	if(dayCounter <= 30){
 		setTimeout(function() { fetchWeather(); }, 2000);
@@ -87,14 +73,12 @@ function fetchWeather(param, callback) {
 	console.log("fetchWeather() is done");
 }
 
-function displayData(){
-	// for(x=1; x <= 30; x++){
-	// 	document.getElementById("day"+x.toString()).innerHTML = x +"<br>"+ "A/C: "+x+ "<br>"+"Heater: "+x;//test
-	// 	// document.getElementById("day"+x.toString()).innerHTML = dateArr[x-1] +"<br>"+ "A/C: "+acArr[x-1]+ "<br>"+"Heater: "+heatArr[x-1];
-	// }
-	console.log("displayData() is done");
-}
-
+/*
+*helper function to increment string numbers
+*this is to help with the missing "0"
+*when incememnting numbers
+*for the API call to forecast.io
+*/
 function incString(string){
 	var num = Number(string);
 	num++;
@@ -105,121 +89,4 @@ function incString(string){
 	else{
 		return str;
 	}
-}
-/*
- * Below are some examples on how to use the data collected in weatherData
- * In my project I used the Skycon HTML5 SVG icon set made by Dark Sky themselves as it's tied into the Forecase.io API.
- *
- * Also, I've used Moment.js to handle my times. You might want to go check that out! :)
- */
-
-// Quick Function: Round number to nearest whole number (optional decimal points)
-function round(number, points) {
-	return number.toFixed(points);
-}
-
-
-/* Current weather conditons
- *
- * This function is a quick example on how you can use the data we've collected in weatherData from fetchWeather().
- * Obvously it's not polished at all, and is totally only a guideline.
- *
- * Example:
- * <weather>
- *   <span class="conditions_current"></span>
- *   <infomation>
- *     <span class="temperature"></span>
- *     <span class="sunrise"></span>
- *     <span class="sunset"></span>
- *     <span class="wind"></span>
- *     <span class="precip_prob"></span>
- *     <span class="humidity"></span>
- *   </infomation>
- * </weather>
- *
- */
-function currentWeather() {
-
-	// Animated Skycon for current conditions
-	//skycons.set("weatherCurrent", weatherData.currently.icon);    // Skycon Icons by Dark Sky
-
-	// Daily summary
-	$('.conditions_current').update(weatherData.hourly.summary);
-
-	// Temperature
-	$('.temp_current').update(round(weatherData.currently.temperature, 1) + '&deg;');
-
-
-
-	// Daily maximum & minimum temperature
-	if (weatherData.flags.units === "us") {
-		$('.temperature').update(round(weatherData.daily.data[0].temperatureMax) + '&deg;' + ' / ' + round(weatherData.daily.data[0].temperatureMin) + '&deg;');
-	} else {
-		$('.temperature').update(round(weatherData.daily.data[0].temperatureMax) + '&deg;' + ' / ' + round(weatherData.daily.data[0].temperatureMin) + '&deg;');
-	}
-
-	// Sunrise
-	$('.sunrise').update(moment.unix(weatherData.daily.data[0].sunriseTime).format('h.mm a'));
-
-	// Sunset
-	$('.sunset').update(moment.unix(weatherData.daily.data[0].sunsetTime).format('h.mm a'));
-
-	// Wind speed + icon (w/ direction rotation)
-	$('.wind').update(round(weatherData.currently.windSpeed) + ' kts', 1000);
-
-	// Precip Probability
-	$('.precip_prob').update(round(weatherData.daily.data[0].precipProbability) + '%');
-
-	// Humidity
-	$('.humidity').update(round(weatherData.daily.data[0].humidity * 100) + '%');
-}
-
-
-/* Forecasted weather conditons
- *
- * This is an example of a loop that we can use, while having a PHP loop setup on the display page
- * to create a weekly forecast. Forecast.io provide data for seven days in advanced.
- *
- * Example:
- * <forecast>
- *   <span class="conditions_future"></span>
- *   <?php
- *     for ($i = 1; $i <= 7; $i++) {
- *       echo "<item>";
- *       echo "	<canvas id='weather$i' class='icon'></canvas>";    // Skycon Icons by Dark Sky
- *       echo "	<span class='day day$i'></span><br />";
- *       echo "	<span class='max max$i'></span>";
- *       echo "	<span class='min min$i'></span>";
- *       echo "</item>";
- *     }
- *   ?>
- * </forecast>
- */
-function forecastWeather() {
-
-	// Weekly forcast summary
-	$('.conditions_future').update(weatherData.daily.summary);
-
-	// Loop through the next 7 days
-	for(var i = 1; i < weatherData.daily.data.length; i++) {
-		var obj = weatherData.daily.data[i];
-		var day = obj.time;
-
-		// Day title
-		var day_day = moment.unix(day).format('dddd');
-		$('.day'+i).update(day_day);
-
-		// Maximum
-		var day_max = round(obj.temperatureMax);
-		$('.max'+i).update(day_max+'&deg;');
-
-		// Minimum
-		var day_min = round(obj.temperatureMin);
-		$('.min'+i).update('/ ' + day_min+'&deg;');
-
-		// Animated Skycon
-		var day_icon = obj.icon;
-		skycons.set("weather"+i, day_icon);
-	}
-
 }
