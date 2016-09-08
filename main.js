5cb3f8e2dce4837bcec0da23b6741ced3aba3b20
Matt <ongmattd@gmail.com>
2016-09-07 23:44:35 -1000

    updated wrapper to do the same as pervious code

--- main.js
+++ main.js
@@ -22,8 +22,14 @@ var dateArr      =  [];
 var acArr 	     =  [];
 var heatArr		 = 	[];
 
+function exeForecast(){
+	fetchWeather("fetching", function(){
+		displayData(callback);
+	});
+}
+
 /* Function: Fetch Forecast.io weather forecast */
-function fetchWeather() {
+function fetchWeather(param, callback) {
 
 	//iterate through day
 	day = incString(day);
@@ -57,16 +63,26 @@ function fetchWeather() {
 			acOn = 0;
 			heaterOn = 0;
 		}
+
 	});
 
 	var stringJson = JSON.stringify(weatherData);
 	console.log("string here "+ stringJson);
 	console.log("check for current temp : "+stringJson.latitude);
-	//console.log("temp for day 1"+ stringJson.hourly.data[0].temperature);
-	//console.log("weatherData.data[0].temperature"+weatherData.data[0].temperature);
-	//document.getElementById("demo").innerHTML = weatherData.temperature;
+	if (callback && typeof(callback) === "function") {
+        callback();
+    }
 	// Fetch the weather every fifteen minutes
-	setTimeout(function() { fetchWeather();  }, 900000);
+	// setTimeout(function() { fetchWeather();  }, 900000);
+	console.log("fetchWeather() is done");
+}
+
+function displayData(){
+	for(x=1; x <= 30; x++){
+		document.getElementById("day"+x.toString()).innerHTML = x +"<br>"+ "A/C: "+x+ "<br>"+"Heater: "+x;//test
+		//document.getElementById("day"+x.toString()).innerHTML = dateArr[x-1] +"<br>"+ "A/C: "+acArr[x-1]+ "<br>"+"Heater: "+heatArr[x-1];
+	}
+	console.log("displayData() is done");
 }
 
 function incString(string){
